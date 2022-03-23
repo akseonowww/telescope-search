38e57d0c805799b307951745a8b866107ff7c6ae
Ruslan Akseonov <54501083+akseonowww@users.noreply.github.com>
2022-03-23 16:23:10 +0300

    Add animation in logo

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,15 @@ import Search from './components/Search/Search'
 import logo from './assets/image/logo.svg'
 
 const App = () => (
-    <div className="App">
-        <div className={styles.Windows}>
-            <div className={styles.Header}>
-                <img className={styles.logoTS} src={logo} alt="logo" />
-                <h1>Телескоп</h1>
-            </div>
-            <Search className={styles.Search} />
-        </div>
-    </div>
+   <div className="App">
+      <div className={styles.Windows}>
+         {/* <div className={styles.Header_active}>
+            <img className={styles.logoTS} src={logo} alt="logo" />
+            <h1>Телескоп</h1>
+         </div> */}
+         <Search className={styles.Search} />
+      </div>
+   </div>
 )
 
 export default App
--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -3,52 +3,55 @@ import Results from '../Results/Results'
 import styles from './Search.module.scss'
 import iconX from '../../assets/image/icon-x.svg'
 import Data from '../Data/Data'
+import logo from '../../assets/image/logo.svg'
 
 const Search = () => {
-    const [value, setValue] = useState('н') // Удалить данные
-    const yandex = Data.searchEngine[0]
-    const defaultLink = `https://${yandex.link + value}`
+   const [value, setValue] = useState('н') // Удалить данные
+   const yandex = Data.searchEngine[0]
+   const defaultLink = `https://${yandex.link + value}`
 
-    const handleKeyPress = (target) => {
-        if (target.charCode === 13) {
-            window.open(defaultLink)
-        }
-    }
+   const handleKeyPress = (target) => {
+      if (target.charCode === 13) {
+         window.open(defaultLink)
+      }
+   }
 
-    const onbuttonHandler = () => {
-        setValue('')
-    }
+   const onbuttonHandler = () => {
+      setValue('')
+   }
 
-    return (
-        <div className={styles['Search-Content']}>
-            <form className={styles.Form}>
-                <div className={styles.Search}>
-                    <input
-                        className={styles['Search-Input']}
-                        placeholder="Найди свою звезду!"
-                        onKeyPress={handleKeyPress}
-                        onChange={(el) => setValue(el.target.value)}
-                        value={value || ''}
-                    />
-                    <div className={styles['Search-Buttons']}>
-                        <button type="button" onClick={onbuttonHandler}>
-                            <img src={iconX} alt="icon delete" />
-                        </button>
-                        <span className={styles.line} />
-                        <button type="button">
-                            <img
-                                src={yandex.icon}
-                                alt="Search engine default"
-                            />
-                        </button>
-                    </div>
-                </div>
-                <div className={styles['Result-List']}>
-                    <Results value={value} />
-                </div>
-            </form>
-        </div>
-    )
+   return (
+      <form className={styles['Search-Content']}>
+         {/* <div className="Header active"> */}
+         <div className={styles.Header}>
+            <img className={styles.logoTS} src={logo} alt="logo" />
+            <h1>Телескоп</h1>
+         </div>
+         <div className={styles.Form}>
+            <div className={styles.Search}>
+               <input
+                  className={styles['Search-Input']}
+                  placeholder="Найди свою звезду!"
+                  onKeyPress={handleKeyPress}
+                  onChange={(el) => setValue(el.target.value)}
+                  value={value || ''}
+               />
+               <div className={styles['Search-Buttons']}>
+                  <button type="button" onClick={onbuttonHandler}>
+                     <img src={iconX} alt="icon delete" />
+                  </button>
+                  <span className={styles.line} />
+                  <button type="button">
+                     <img src={yandex.icon} alt="Search engine default" />
+                  </button>
+               </div>
+            </div>
+            <div className={styles['Result-List']}>
+               <Results value={value} />
+            </div>
+         </div>
+      </form>
+   )
 }
 
 export default Search
