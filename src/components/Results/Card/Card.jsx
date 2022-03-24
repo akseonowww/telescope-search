cddfd3e6af34746fe19a23867425abd93db620be
Ruslan Akseonov <54501083+akseonowww@users.noreply.github.com>
2022-03-24 22:28:56 +0300

    Fix adaptive

--- src/components/Results/Card/Card.jsx
+++ src/components/Results/Card/Card.jsx
@@ -5,83 +5,78 @@ import iconOpen from '../../../assets/image/icon-open-card.svg'
 import Data from '../../Data/Data'
 
 const Card = ({ value, title, info, icon }) => {
-    const classComponent = 'Card'
+   const classComponent = 'Card'
+   const titleNoHTML = title.replace('<mark>', '', 1).replace('</mark>', '', 1)
 
-    const [isActive, setActive] = useState(false)
+   const [isActive, setActive] = useState(false)
 
-    const openCard = () => {
-        setActive(!isActive)
-    }
+   const openCard = () => {
+      setActive(!isActive)
+   }
 
-    const wiki = Data.searchEngine[3]
+   const wiki = Data.searchEngine[3]
 
-    return (
-        <div
-            className={`${styles[`${classComponent}`]} ${
-                isActive ? styles.active : styles.no_active
-            }`}
-        >
-            <a
-                href={`https://${wiki.link + title}`}
-                className={`${styles[`${classComponent}__info`]}`}
-            >
-                <div className={styles[`${classComponent}__icon`]}>
-                    <div
-                        className={styles[`${classComponent}__icon-container`]}
-                    >
-                        <img
-                            className={styles[`${classComponent}__icon-img`]}
-                            src={icon}
-                            alt={`icon ${value}`}
-                        />
-                    </div>
-                </div>
-                <div className={styles[`${classComponent}__text`]}>
-                    <span dangerouslySetInnerHTML={{ __html: title }} />
-                    <span
-                        className={styles[`${classComponent}__text-info`]}
-                        dangerouslySetInnerHTML={{ __html: info }}
-                    />
-                </div>
-            </a>
-            <div className={styles[`${classComponent}__option`]}>
-                <div className={styles[`${classComponent}__option-btn_hidden`]}>
-                    <a
-                        className={styles[`${classComponent}__option-btn`]}
-                        href={`https://${wiki.link + title}`}
-                    >
-                        <div
-                            className={
-                                styles[`${classComponent}__option-btn__img`]
-                            }
-                        >
-                            {/* <img src={wiki.icon} alt="icon wikipedia" /> */}
-                        </div>
-                        Произведения (826)
-                    </a>
-                </div>
-                <a
-                    className={styles[`${classComponent}__option-btn`]}
-                    href={`https://${wiki.link + title}`}
-                >
-                    <div
-                        className={styles[`${classComponent}__option-btn__img`]}
-                    >
-                        <img src={wiki.icon} alt="icon wikipedia" />
-                    </div>
-                    Википедея
-                </a>
-                <button
-                    className={styles[`${classComponent}__option-open`]}
-                    type="button"
-                    onClick={() => openCard()}
-                >
-                    <img src={iconOpen} alt="icon open list" />
-                </button>
+   return (
+      <div
+         className={`${styles[`${classComponent}`]} ${
+            isActive ? styles.active : styles.no_active
+         }`}
+      >
+         <a
+            href={`https://${wiki.link + title}`}
+            className={`${styles[`${classComponent}__info`]}`}
+         >
+            <div className={styles[`${classComponent}__icon`]}>
+               <div className={styles[`${classComponent}__icon-container`]}>
+                  <img
+                     className={styles[`${classComponent}__icon-img`]}
+                     src={icon}
+                     alt={`icon ${value}`}
+                  />
+               </div>
             </div>
-            {/* </a> */}
-        </div>
-    )
+            <div className={styles[`${classComponent}__text`]}>
+               <span dangerouslySetInnerHTML={{ __html: title }} />
+               <span
+                  className={styles[`${classComponent}__text-info`]}
+                  dangerouslySetInnerHTML={{ __html: info }}
+               />
+            </div>
+         </a>
+         <div className={styles[`${classComponent}__option`]}>
+            <div className={styles[`${classComponent}__option-btn_hidden`]}>
+               <a
+                  className={styles[`${classComponent}__option-btn`]}
+                  href={`https://${wiki.link + title}`}
+               >
+                  <div className={styles[`${classComponent}__option-btn__img`]}>
+                     {/* <img src={wiki.icon} alt="icon wikipedia" /> */}
+                  </div>
+                  Произведения (826)
+               </a>
+            </div>
+            <a
+               className={styles[`${classComponent}__option-btn`]}
+               href={`https://${wiki.link + titleNoHTML}`}
+            >
+               <div className={styles[`${classComponent}__option-btn__img`]}>
+                  <img src={wiki.icon} alt="icon wikipedia" />
+               </div>
+               <span className={styles[`${classComponent}__option-btn__text`]}>
+                  Википедея
+               </span>
+            </a>
+            <button
+               className={styles[`${classComponent}__option-open`]}
+               type="button"
+               onClick={() => openCard()}
+            >
+               <img src={iconOpen} alt="icon open list" />
+            </button>
+         </div>
+         {/* </a> */}
+      </div>
+   )
 }
 
 export default Card
