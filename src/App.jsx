8e50a8cc34b35af7215b3ed84f3d91b7c6c64cba
Ruslan Akseonov <54501083+akseonowww@users.noreply.github.com>
2022-04-10 20:45:46 +0300

    Add styles in TS

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,10 @@ import { Tab, Tabs, TabList, TabPanel } from 'react-tabs'
 import 'react-tabs/style/react-tabs.css'
 import './App.scss'
 import Search from './components/Search/Search'
+import Data from './store/data'
 import Sdamgia from './Themes/Sdamgia/Sdamgia'
 import Telescope from './Themes/Telescope/Telescope'
+import logoTS from './assets/image/logo.svg'
 import Yandex from './Themes/Yandex/Yandex'
 // import dataTS from './store/data'
 // import data from './store/data'
@@ -14,6 +16,7 @@ import Yandex from './Themes/Yandex/Yandex'
 const App = () => {
    const baseURL = 'https://jsonblob.com/api/jsonBlob/962331985410539520' // 30 days (9.04-...)
    // const [urlData, setUrlData] = useState(baseURL)
+   const { bookmarks } = Data
 
    // let baseData = 0
 
@@ -27,22 +30,16 @@ const App = () => {
          <Tabs forceRenderTabPanel defaultIndex={0}>
             <TabList>
                <Tab>
-                  <img
-                     src="https://telescope-search.vercel.app/static/media/logo.36c9d5d4d5e780cf00a7c3ef979205f8.svg"
-                     alt="Лого"
-                  />
+                  <img src={logoTS} alt="Лого" />
                   <span>Телескоп</span>
                </Tab>
                <Tab>
-                  <img
-                     src="https://ege.sdamgia.ru/img/headers/logo.svg"
-                     alt="Лого"
-                  />
+                  <img src={bookmarks[5].icon} alt="Лого" />
                   <span>СДАМ.ГИА</span>
                </Tab>
                <Tab>
                   <img
-                     src="https://uc6f299b1f06d6a82aa9563cfd60.previews.dropboxusercontent.com/p/thumb/ABfcONBgIKWwWsq9x4kF5MyVLrPIi2hn6y8m9vGyYrNX0EFw6x-U-PEg3S0RkJapyGo0eR9sibclQR5fKeeQxPwgVvoTJVGirG6fOfPN744o9IoE4-sP-QmirJ_IjlM1DD_4Ii2Kvva--UBK0nSph_fDmf49LW8_4QBrsg5bblYQBHUO8klalikMX8raKjPdaqdexnvXNnnkU8_R4BzvNkbw_1iQNgV3uH-jcAD9tOCIuC52Hy6HkCpepnrJSCEYRLCbOGIe107cjxLp8sRMsY17kqFcSULTkjzO7N0cljll0czcy-xdgHUyx15G7jSKLY073fZ9KrJ4jFmfQ0hS74_oGOMwRZEsdWhntORWHFRBkwfOjXoPFreOz2QR244-DFdrU_Hd7kl4WyuVBQMAFT61hnzMUJsp89XeHF4oWQcH2A/p.png"
+                     src={bookmarks[4].icon}
                      alt="Лого"
                   />
                   <span>Яндекс</span>
--- src/store/data.js
+++ src/store/data.js
@@ -1,3 +1,11 @@
+import gorkiiPhoto from '../assets/image/authors/gorkii.jpg'
+import lermontovPhoto from '../assets/image/authors/lermontov.jpg'
+import pushkinPhoto from '../assets/image/authors/pushkin.jpg'
+
+import biblePhoto from '../assets/image/books/bible.jpg'
+import kapitalPhoto from '../assets/image/books/kapital.jpg'
+import wePhoto from '../assets/image/books/we.jpg'
+
 import yandexIcon from '../assets/image/bookmarks/icon-yandex.svg'
 import vkIcon from '../assets/image/bookmarks/icon-vk.svg'
 import googleIcon from '../assets/image/bookmarks/icon-google.png'
@@ -6,6 +14,7 @@ import wikiIcon from '../assets/image/bookmarks/icon-wikipedia.svg'
 import tinkoffIcon from '../assets/image/bookmarks/icon-tinkoff.svg'
 import youtubeIcon from '../assets/image/bookmarks/icon-youtube.svg'
 import instagramIcon from '../assets/image/bookmarks/icon-instagram.svg'
+import sdamgiaIcon from '../assets/image/bookmarks/icon-sdamgia.svg'
 
 const Data = {
    searchEngine: [
@@ -56,7 +65,12 @@ const Data = {
          link: "yandex.ru",
          icon: yandexIcon
 
-      }
+      },
+      {
+         name: "СДАМ.ГИА",
+         link: "sdamgia.ru",
+         icon: sdamgiaIcon
+      },
    ],
    cards: [
       {
@@ -66,19 +80,19 @@ const Data = {
                name: "Александр Сергеевич Пушкин",
                "yaerLife": "1799-1837",
                info: "Русский поэт, драматург и прозаик, заложивший основы русского реалистического направления, литературный критик и теоретик литературы, историк, публицист, журналист; один из самых авторитетных литературных деятелей первой трети XIX века.",
-               img: "https://downloader.disk.yandex.ru/preview/4c5017179b9794fa409f7160304579aabf010514238149adee1b1e7d12deeb0c/6250653e/9JGEuKEMpjainuWLTCU8tetXeaU2cAeoBCEllcfHvgoCChin49EXDB0U435Jo3m2FRejja5xTNVWmFmUq77LXQ%3D%3D?uid=0\u0026filename=pushkin.jpg\u0026disposition=inline\u0026hash=\u0026limit=0\u0026content_type=image%2Fjpeg\u0026owner_uid=0\u0026tknv=v2\u0026size=1860x947"
+               img: pushkinPhoto
             },
             {
                name: "Максим Горький",
                "yaerLife": "1868-1936",
                info: "Русский и советский писатель, поэт, прозаик, драматург, основоположник литературы социалистического реализма, инициатор создания Союза писателей СССР и первый председатель правления этого союза. ",
-               img: "https://4.downloader.disk.yandex.ru/preview/3f75a1e8ec2576c6df12d25d6295a520420e5cbd01c8bb331597350f0e5e40df/inf/5klqrfi8sbCuryZKWuVM8cQCkrNlkiFVc6ymHOLcoferWjGivhYvVqLV1UXpNBfsA0x60vDVkA1q977AVpibWg%3D%3D?uid=1473078644\u0026filename=gorkii.jpg\u0026disposition=inline\u0026hash=\u0026limit=0\u0026content_type=image%2Fjpeg\u0026owner_uid=1473078644\u0026tknv=v2\u0026size=1860x947"
+               img: gorkiiPhoto
             },
             {
                name: "Михаил Юрьевич Лермонтов",
                "yaerLife": "1814-1841",
                info: "Русский поэт, прозаик, драматург, художник. ",
-               img: "https://3.downloader.disk.yandex.ru/preview/24d20a0e7c8e6d7f39a25eaa6ca8206910a788d66e472643d0f5174919b052cf/inf/U6sfdNBwHmS_yhTpHRdfw07-T48_a1ZABF4AeRlXhHS04u5ic0BxLJSawqwwLPE9i8DvLvdk3n3lcl3RqoCAnw%3D%3D?uid=1473078644\u0026filename=lermontov.jpg\u0026disposition=inline\u0026hash=\u0026limit=0\u0026content_type=image%2Fjpeg\u0026owner_uid=1473078644\u0026tknv=v2\u0026size=1860x947"
+               img: lermontovPhoto
             }
          ]
       },
@@ -89,19 +103,19 @@ const Data = {
                name: "Библия",
                info: "Собрание текстов, являющихся священными в иудаизме и христианстве и составляющих Священное Писание в этих религиях. В иудаизме Священным Писанием является Танах, именуемый также еврейской Библией; в христианстве - Ветхий Завет, состоящий из книг Танаха и дополнительных священных книг, и Новый Завет. Книги Танаха составляют иудейский канон. Книги Танаха и Нового Завета являются каноническими во всех христианских конфессиях. Различия в Библиях различных Церквей и конфессий состоят в наличии дополнительных книг и отрывков в Ветхом Завете и в некотором отличии перевода. В православии эти дополнительные книги и отрывки названы неканоническими или «анагиноскомена»; в католицизме - второканоническими; в протестантизме - апокрифическими.",
                yearWrite: "-0",
-               img: "https://3.downloader.disk.yandex.ru/preview/9c09465fc9da4844c2a9cf90a95f14bff8c56440e14c7e2ed33fc03cf11be9d6/inf/MDOh2rQv0XRxtPZOy57FmcQCkrNlkiFVc6ymHOLcoffvB2y3rqnimi1rhdbESu1Q4XbqF8S6J99zmFlmvpRxNA%3D%3D?uid=1473078644\u0026filename=bible.jpg\u0026disposition=inline\u0026hash=\u0026limit=0\u0026content_type=image%2Fjpeg\u0026owner_uid=1473078644\u0026tknv=v2\u0026size=1860x947"
+               img: biblePhoto
             },
             {
                name: "Капитал",
                info: "Главный труд немецкого экономиста Карла Маркса по политической экономии, содержащий критический анализ капитализма. Работа написана с применением диалектико-материалистического подхода, в том числе к историческим процессам.",
                yearWrite: "1867",
-               img: "https://3.downloader.disk.yandex.ru/preview/4e47a6bc10ad888106e46385f5ff7eacd34ac3ddef75ed97708c0245e99b7b2c/inf/koZ6sWIb0NEe0gzerKsncEOEMEku1ba41DPk9k1Rryige0CitZ0Rr19KEB3v6WoPQgg_e5PuYZCZGL7eYqrjog%3D%3D?uid=1473078644\u0026filename=kapital.jpg\u0026disposition=inline\u0026hash=\u0026limit=0\u0026content_type=image%2Fjpeg\u0026owner_uid=1473078644\u0026tknv=v2\u0026size=1860x947"
+               img: kapitalPhoto
             },
             {
                name: "МЫ, Евгений Замятин",
                info: "Роман-антиутопия Евгения Замятина, написанный в 1920 году. В СССР не печатался до 1988 года как «идеологически враждебное» и «клеветническое» произведение. Повлиял на творчество многих известных писателей XX века, в том числе на Олдоса Хаксли, Джорджа Оруэлла, Курта Воннегута и Владимира Набокова.",
                yearWrite: "1957",
-               img: "https://2.downloader.disk.yandex.ru/preview/3e18d7c8cae26c603acd2114aeaa8df5634502f9cca12a18904efc7c9a70b6a6/inf/a-ANsF5JdKA7vYsl4RhJj0VBsMMiXApVPg_aUBCuK7wzj2bxHcI-Yz3CKG7z6Ick0uHWKDoDgacFSHygZRrr_A%3D%3D?uid=1473078644\u0026filename=we.jpg\u0026disposition=inline\u0026hash=\u0026limit=0\u0026content_type=image%2Fjpeg\u0026owner_uid=1473078644\u0026tknv=v2\u0026size=1860x947"
+               img: wePhoto
             }
          ]
       }
@@ -128,27 +142,27 @@ const Data = {
                   }
                ]
             },
-            { name: "Физика", url: "../assets/image/" },
-            { name: "Химия", url: "../assets/image/" },
-            { name: "Информатика", url: "../assets/image/" }
+            { name: "Физика", url: "/" },
+            { name: "Химия", url: "/" },
+            { name: "Информатика", url: "/" }
          ]
       },
       {
          name: "Экзамен",
          group: [
-            { name: "ЕГЭ", url: "../assets/image/" },
-            { name: "ОГЭ", url: "../assets/image/" },
-            { name: "ВПР", url: "../assets/image/" },
-            { name: "ГВЭ", url: "../assets/image/" },
-            { name: "ЦТ", url: "../assets/image/" }
+            { name: "ЕГЭ", url: "/" },
+            { name: "ОГЭ", url: "/" },
+            { name: "ВПР", url: "/" },
+            { name: "ГВЭ", url: "/" },
+            { name: "ЦТ", url: "/" }
          ]
       },
       {
          name: "Софт",
          group: [
-            { name: "Карточки", url: "../assets/image/" },
-            { name: "Соревнование", url: "../assets/image/" },
-            { name: "Калькулятор", url: "../assets/image/" }
+            { name: "Карточки", url: "/" },
+            { name: "Соревнование", url: "/" },
+            { name: "Калькулятор", url: "/" }
          ]
       }
    ]
